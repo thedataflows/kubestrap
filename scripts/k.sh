e70a33fef4c5b5b8126d4cc7bd276711f8d1253b
Cristian Chiru <cristi.chiru@gmail.com>
2023-09-27 07:44:30 +0300

    improve wrapper script

--- scripts/k.sh
+++ scripts/k.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
-SCRIPT_DIR=$(readlink -f "$0")
-SCRIPT_DIR="${SCRIPT_DIR%/*}"
-
 PACKAGE_NAME=kubestrap
 PACKAGE_VERSION="${PACKAGE_VERSION:-v0.0.0}"
 PACKAGE_BIN=${PACKAGE_NAME}
 PACKAGE_REPO=https://github.com/thedataflows/kubestrap
+
+SCRIPT_DIR=$(readlink -f "$0")
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
 if [[ "$OS" == "Windows_NT" ]]; then
     [[ $(type -p cygpath) ]] && SCRIPT_DIR=$(cygpath -u "$SCRIPT_DIR")
@@ -34,5 +34,5 @@ if [[ ! $(type -p $PACKAGE_BIN) || "$($PACKAGE_BIN version)" != "$PACKAGE_VERSIO
 fi
 
 set -x
-$PACKAGE_BIN "$@"
+$PACKAGE_BIN --config $SCRIPT_DIR/kubestrap-defaults.yaml "$@"
 { set +x; } 2>/dev/null
